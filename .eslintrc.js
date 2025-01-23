f5ed8a746e38c274864c9f7228fbdf4e689790a7
Imam Abdillah <92290516+imamabdillah@users.noreply.github.com>
2025-01-23 16:44:25 +0700

    Create .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+    rules: {
+      "@typescript-eslint/no-unused-vars": "off", // Nonaktifkan aturan "no-unused-vars"
+      "react/no-unescaped-entities": "off", // Nonaktifkan aturan "no-unescaped-entities"
+    },
+  };
+  
